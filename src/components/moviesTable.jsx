981cd2de6a59c01e5fbcba018d7e28242d4d459b
Stefan Miloiu <stefanmiloiu@live.com>
2022-10-06 21:52:31 +0300

    created loginForm and integrated it in the navbar

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 import Like from "./common/like";
 import Table from "./common/table";
+import { Link } from "react-router-dom";
 
 class MoviesTable extends Component {
   columns = [
-    { path: "title", label: "Title" },
+    {
+      path: "title",
+      label: "Title",
+      content: (movie) => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>,
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
     {
       key: "like",
-      content: (movie) => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />,
+      content: (movie) => (
+        <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
+      ),
     }, //for like column
     {
       key: "delete",
       content: (movie) => (
-        <button onClick={() => this.props.onDelete(movie)} className="btn btn-danger btn-sm">
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
           Delete
         </button>
       ),
@@ -25,7 +35,14 @@ class MoviesTable extends Component {
   render() {
     const { movies, onSort, sortColumn } = this.props;
 
-    return <Table columns={this.columns} data={movies} sortColumn={sortColumn} onSort={onSort} />;
+    return (
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
+    );
   }
 }
 
